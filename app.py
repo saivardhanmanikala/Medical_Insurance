4c918af0d7e3de0c91b368c887e017a8906f3e19
saivardhanmanikala <119521126+saivardhanmanikala@users.noreply.github.com>
2025-04-05 10:33:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,7 @@ def predict():
 if __name__ == "__main__":
     print("🔥 Server is running! Use /predict for predictions.")  
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port)
+
 
